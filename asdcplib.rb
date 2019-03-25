9f099bcca5d5d4fc41879614685dde05924a8615
Marc-Antoine ARNAUD <MarcAntoine-Arnaud@users.noreply.github.com>
2019-03-25 11:31:43 +0100

    Update asdcplib.rb

--- asdcplib.rb
+++ asdcplib.rb
@@ -15,7 +15,7 @@ class Asdcplib < Formula
   depends_on "xerces-c"
 
   def install
-    system "cmake", "-DOpenSSLLib_include_DIR=/usr/local/opt/openssl/include", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_BUILD_TYPE=RELEASE", "."
+    system "cmake", "-DOpenSSLLib_include_DIR=/usr/local/opt/openssl/include", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_BUILD_TYPE=RELEASE"
     system "make"
     system "make", "install"
   end
